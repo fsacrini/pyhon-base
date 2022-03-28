4cec0de8d790715d13be897a2f22c78b987f9ddd
Fabio Sacrini <fsacrini@gmail.com>
2022-03-28 18:51:33 -0400

    Adding more info

--- hello.py
+++ hello.py
@@ -1,8 +1,45 @@
 #!/usr/bin/env python3
-# Comment to remember Python Scripting
+""" Nome do Programa: Hello World Multi Linguas.
 
-print("Hello, World!")
-print('fabio'.upper())
-print(40 + 82)
+Até coluna 80
+Descricao: Depenendo da lingua configurada no ambiente o programa exibe a 
+mensagem correspondende.
+
+Usage:
+Tenha a váriavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução
+
+    python3 hello.py
+    or
+    ./hello.py
+"""
+__version__ = "0.0.1"
+__author__ = "Fabio Sacrini"
+__license__ = "Unlicense"
+
+import os
+
+#Dunder => __SOMETHING__
+
+current_language = "en_US" 
+current_language = "pt_BR"
+current_language = "it_IT"
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+#Snake Case = current_language
+#Pascal Case = CurrentLanguage
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
+#print('fabio'.upper())
+#Print(40 + 82)
 
 #Test
