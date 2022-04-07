3edd49e3a28a7e1ae69c118851725331f229723a
Fabio Sacrini <fsacrini@gmail.com>
2022-04-07 17:43:25 -0400

    Add Comment

--- escola_v3_com_dict.py
+++ escola_v3_com_dict.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python3
-"""
-"""
+""" Separacao de classes por sala
 
+Descricao: O programa ira verificar todos os alunos separado por salas de aula.
+Importante que o nome esteja correto e igual.
+
+Usage:
+Caso inclua uma nova sala ou uma nova classe, crie uma nova linha no dicionario
+alunos ou classes e os alunos tem que estar dentro de uma lista.
+
+Execução
+
+    python3 escola_v3_com_dict.py
+"""
+__version__ = "0.3.0"
+__author__ = "Fabio Sacrini"
+__license__ = "Unlicense"
 
 # Dados
 alunos = {
